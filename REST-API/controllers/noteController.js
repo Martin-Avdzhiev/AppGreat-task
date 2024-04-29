8ba99e11c72dad2741f3696c1d7f01b6099a5910
AndreyRRaychev <166109963+AndreyRRaychev@users.noreply.github.com>
2024-04-29 16:05:12 +0300

    add crud apis on notes

--- REST-API/controllers/noteController.js
+++ REST-API/controllers/noteController.js
@@ -1,3 +1,40 @@
 const Note = require('../models/Note.js');
 const noteRouter = require('express').Router();
 
+
+noteRouter.post('/create',async (req, res) => {
+    try {
+        const noteData = req.body;
+        const note = await Note.findOne(noteData);
+      return  res.status(200).send(note);
+    } catch (error) {
+        console.log(error.message);
+       return res.status(400).send(JSON.stringify({error:error.message}));
+    }
+    
+});
+noteRouter.put('/edit/:id',async (req, res) => {
+    try {
+        const id = req.params.id;
+        const noteData = req.body;
+        const note = await Note.findByIdAndUpdate(id,noteData);
+      return  res.status(200).send(note);
+    } catch (error) {
+        console.log(error.message);
+       return res.status(400).send(JSON.stringify({error:error.message}));
+    }
+    
+});
+noteRouter.delete('/delete/:id',async (req, res) => {
+    try {
+        const id = req.params.id;
+        const note = await Note.findByIdAndDelete(id);
+      return  res.status(200).send(note);
+    } catch (error) {
+        console.log(error.message);
+       return res.status(400).send(JSON.stringify({error:error.message}));
+    }
+    
+});
+
+module.exports = noteRouter;
